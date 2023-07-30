6de168291f2101edbb30e414c01fbe538d43ba49
nicojpalacio <nicojpalacio@gmail.com>
2023-07-30 19:30:04 -0300

    form-created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,25 @@
-import { Form } from "../components/Form"
+import { useState } from 'react'
+import '../App.css'
 
 export const Login = () => {
-  return (
-    <Form/>
+  const [login, setLogin] = useState("false")
+
+    return (
+    <div className="container login">
+        
+            <form id="form_login">
+                <div>
+                    <h1 >LOGIN</h1>
+                    <label htmlFor="txtusu"><strong>Username</strong></label>
+                    <input type="text" id="txtusu"  className="form-control"  required/>
+                </div>
+                <div>
+                    <label htmlFor="txtpas"><strong>Password</strong></label>
+                    <input type="password" id="txtpas"  className="form-control"  required/>
+                </div><br/>
+                <input type="submit"  className="btn btn-primary" value="Login"/>
+            </form>
+
+        </div>
   )
 }
